e5829ffbe7009e47718d42785c9dbb61a71b02dd
insidecode-dev <trqtmhdv@gmail.com>
2024-01-19 18:09:38 +0400

    quick sort

--- sort.h
+++ sort.h
@@ -29,6 +29,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void _swap(listint_t **node, listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
 
 #endif
